ecf7b4ae7920e1853c222aead2cc39ec6ab52d4c
bmaz <beatrice.mazoyer@gmail.com>
2023-07-27 14:44:01 +0200

    Create build folder

--- src/build.js
+++ src/build.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
+const rm = require("rimraf");
 const path = require("path");
-const fetch = require("node-fetch");
+const fse = require("fs-extra");
 const csv = require("csvtojson");
+const fetch = require("node-fetch");
 const nunjucks = require("nunjucks");
 const pandemonium = require("pandemonium");
 
@@ -9,9 +11,9 @@ module.exports = function build() {
   main();
 };
 
-const env = nunjucks.configure([
-  path.join(__dirname, "..", "site", "templates"),
-]);
+const baseUrl = path.join(__dirname, "..", "build");
+const siteUrl = path.join(__dirname, "..", "site");
+const env = nunjucks.configure(path.join(siteUrl, "templates"));
 const template = env.getTemplate("template.html");
 const formUrl =
   "https://docs.google.com/spreadsheets/d/1Wner4VHEAGfxmqJ5uLT-n5PJoZKYLUzLs9-_J1PMfq0/export?exportFormat=csv";
@@ -23,6 +25,7 @@ async function main() {
   const output = template.render({
     items: pandemonium.shuffle(items),
   });
-  fs.writeFileSync("index.html", output);
-  console.log("Wrote index.html");
+  rm.sync(baseUrl);
+  fse.copySync(siteUrl, baseUrl);
+  fs.writeFileSync(path.join(baseUrl, "index.html"), output);
 }
