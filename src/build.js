019ccd355f6437e8b7e48399e5af2b3fbad8b939
bmaz <beatrice.mazoyer@gmail.com>
2023-07-27 18:30:16 +0200

    Create individual pages for members

--- src/build.js
+++ src/build.js
@@ -13,8 +13,11 @@ module.exports = function build() {
 
 const baseUrl = path.join(__dirname, "..", "build");
 const siteUrl = path.join(__dirname, "..", "site");
+
 const env = nunjucks.configure(path.join(siteUrl, "templates"));
-const template = env.getTemplate("template.html");
+const main_page_template = env.getTemplate("main_page.html");
+const member_page_template = env.getTemplate("member_page.html");
+
 const formUrl =
   "https://docs.google.com/spreadsheets/d/1Wner4VHEAGfxmqJ5uLT-n5PJoZKYLUzLs9-_J1PMfq0/export?exportFormat=csv";
 
@@ -35,11 +38,22 @@ async function main() {
   const body = await response.text();
   const items = await csv().fromString(body);
   const cleanItems = cleanForm(items);
-  const output = template.render({
-    items: cleanItems,
-  });
-  fs.removeSync(baseUrl);
-  fs.outputFileSync(path.join(baseUrl, "index.html"), output);
+  fs.outputFileSync(
+    path.join(baseUrl, "index.html"),
+    main_page_template.render({
+      items: cleanItems,
+    })
+  );
+
+  for (member of cleanItems) {
+    fs.outputFileSync(
+      path.join(baseUrl, member.lastName + ".html"),
+      member_page_template.render({
+        member: member,
+      })
+    );
+  }
+
   fs.outputJSONSync(path.join(baseUrl, "assets", "members.json"), cleanItems, {
     spaces: 2,
   });
