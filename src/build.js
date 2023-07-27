619c7ca27246719cfcfd7080d1263004b739be56
bmaz <beatrice.mazoyer@gmail.com>
2023-07-27 14:05:59 +0200

    Fix errors

--- src/build.js
+++ src/build.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const fetch = require("node-fetch");
 const csv = require("csvtojson");
 const nunjucks = require("nunjucks");
@@ -9,22 +10,20 @@ module.exports = function build() {
   main();
 };
 
-var env = nunjucks.configure(["./", "./site/templates/"]);
-var template = env.getTemplate("template.html");
+const env = nunjucks.configure([
+  path.join(__dirname, "..", "site", "templates"),
+]);
+const template = env.getTemplate("template.html");
 const formUrl =
   "https://docs.google.com/spreadsheets/d/1Wner4VHEAGfxmqJ5uLT-n5PJoZKYLUzLs9-_J1PMfq0/export?exportFormat=csv";
 
 async function main() {
   const response = await fetch(formUrl);
   const body = await response.text();
-  var items = await csv().fromString(body);
-  var output = template.render({
+  const items = await csv().fromString(body);
+  const output = template.render({
     items: shuffle(items),
   });
-  fs.writeFile("index.html", output, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log("Wrote file index.html");
-  });
+  fs.writeFileSync("index.html", output);
+  console.log("Wrote index.html");
 }
