32c32e4bde86b437e70790903b17106f50006f12
bmaz <beatrice.mazoyer@gmail.com>
2023-08-17 17:22:21 +0200

    Create search bar

---
+++ site/js/loadMembers.js
@@ -0,0 +1,13 @@
+function loadJSON(callback) {
+  var request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open("GET", "assets/members.json", true);
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == "200") {
+      callback(request.responseText);
+    }
+  };
+  request.send(null);
+}
+
+module.exports = loadJSON;
--- site/js/search.js
+++ site/js/search.js
@@ -1,3 +1,41 @@
-import shuffle from "pandemonium/shuffle";
+import loadJSON from "./loadMembers";
 
-console.log(shuffle([1, 2, 3, 4, 5]));
+const searchInput = document.querySelector(".input");
+
+function clearList(list) {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+function setList(searchResults) {
+  const ul = document.querySelector("ul");
+  clearList(ul);
+
+  for (const person of searchResults) {
+    const resultItem = document.createElement("li");
+
+    const a = document.createElement("a");
+    a.href = person.slug;
+    a.innerHTML = person.firstName + " " + person.lastName;
+
+    resultItem.appendChild(a);
+    ul.appendChild(resultItem);
+  }
+}
+
+loadJSON(function (response) {
+  const members = JSON.parse(response);
+
+  searchInput.addEventListener("input", (e) => {
+    let value = e.target.value;
+    if (value && value.trim().length > 0) {
+      value = value.trim().toLowerCase();
+      setList(
+        members.filter((person) => {
+          return person.firstName.toLowerCase().includes(value);
+        })
+      );
+    }
+  });
+});
